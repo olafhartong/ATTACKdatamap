035b2c0187195eef0c608ad3791978c7faa6fec1
Olaf Hartong <8149899+olafhartong@users.noreply.github.com>
2019-04-06 22:55:32 +0200

    Initial commit

---
+++ ATTACKdatamap.psd1
@@ -0,0 +1,59 @@
+@{
+    # If authoring a script module, the RootModule is the name of your .psm1 file
+    RootModule = 'ATTACKdatamap.psm1'
+    Author = 'Olaf Hartong <ATTACK@olafhartong.nl>'
+    ModuleVersion = '1.0'
+    GUID = '5436b2bb-91af-4a6e-91eb-39667a8f6b41'
+    Copyright = '2019 Olaf Hartong'
+    CompanyName = 'Olaf Hartong'
+    Description = 'A datasource assessment on an event level to show potential ATT&CK coverage'
+
+    # Minimum PowerShell version supported by this module (optional, recommended)
+    # PowerShellVersion = ''
+
+    # Which PowerShell Editions does this module work with? (Core, Desktop)
+    CompatiblePSEditions = @('Desktop', 'Core')
+
+    # Which PowerShell functions are exported from your module? (eg. Get-CoolObject)
+    FunctionsToExport = @(
+        'Request-ATTACKjson',
+        'Invoke-ATTACK-UpdateExcel',
+        'Get-ATTACKdata'
+    )
+
+    
+    # Which PowerShell aliases are exported from your module? (eg. gco)
+    AliasesToExport = @('*')
+
+    # List of all files packaged with this module
+    FileList = @(
+    'ATTACKdatamap.psm1'
+    )
+
+    # Which PowerShell variables are exported from your module? (eg. Fruits, Vegetables)
+    VariablesToExport = @('*')
+
+    # PowerShell Gallery: Define your module's metadata
+    PrivateData = @{
+        PSData = @{
+            # What keywords represent your PowerShell module? (eg. cloud, tools, framework, vendor)
+            Tags = @('DFIR', 'ThreatHunting')
+
+            # What software license is your code being released under? (see https://opensource.org/licenses)
+            LicenseUri = ''
+
+            # What is the URL to your project's website?
+            ProjectUri = ''
+
+            # What is the URI to a custom icon file for your project? (optional)
+            IconUri = ''
+
+            # What new features, bug fixes, or deprecated features, are part of this release?
+            ReleaseNotes = @'
+'@
+        }
+    }
+
+    # If your module supports updateable help, what is the URI to the help archive? (optional)
+    # HelpInfoURI = ''
+}
---
+++ ATTACKdatamap.psm1
@@ -0,0 +1,194 @@
+function Get-ATTACKdata {
+    <#
+    .SYNOPSIS
+        Downloads the MITRE ATT&CK Enterprise JSON file
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> Get-ATTACKdata -AttackPath ./enterprise-attack.json
+    .OUTPUTS
+        $AttackPath = The location where the ATT&CK Enterprise file will be stored, default is .\enterprise-attack.json
+    #>
+    param (
+        # Log name of where to look for the PowerShell events.
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $AttackPath = 'enterprise-attack.json'
+    )
+    $url = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json" 
+      
+    if(!(Split-Path -parent $AttackPath) -or !(Test-Path -pathType Container (Split-Path -parent $AttackPath))) { 
+      $AttackPath = Join-Path $pwd (Split-Path -leaf $AttackPath) 
+    } 
+      
+    Write-Host "[++] Downloading [$url]`nSaving at [$AttackPath]" -ForegroundColor Cyan
+    $client = new-object System.Net.WebClient 
+    $client.DownloadFile($url, $AttackPath) 
+      
+    $AttackPath  
+}
+
+function Invoke-ATTACK-UpdateExcel {
+    <#
+    .SYNOPSIS
+        Generates MITRE ATT&CK relevant fields into a table and creates or updates a worksheet in an Excel sheet
+        Requires module ImportExcel, Install it like this PS C:\> Install-Module ImportExcel
+    .DESCRIPTION
+    .EXAMPLE
+        PS C:\> Invoke-ATTACK-UpdateExcel -AttackPath .\enterprise-attack.json -Excelfile .\mitre_data_assessment.xlsx
+    .INPUTS
+        AttackPath = The location of the ATT&CK Enterprise JSON file, default is .\enterprise-attack.json
+    .OUTPUTS
+        $Excefile = The location of the Excel file in which you want to create/update the DataSources reference workbook, default is .\mitre_data_assessment.xlsx
+    .NOTES
+    #>
+    param (
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]    
+        $Excelfile = 'mitre_data_assessment.xlsx',
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $AttackPath = 'enterprise-attack.json'
+    )
+    $dataset=Get-Content -Path $AttackPath | ConvertFrom-Json | Select-Object -ExpandProperty objects  | Where-Object type -eq "attack-pattern" 
+
+    $Collection =@()
+    foreach ($object in $dataset)
+        {        
+        $Props = @{
+            'ID' = $object.external_references.'external_id'
+            'Data Source' = $object.'x_mitre_data_sources'
+            'Name' = $object.'name'
+            'Detection' = $object.'x_mitre_detection'
+            'Platforms' = $object.'x_mitre_platforms'
+            'Description' = $object.'description'
+            'Tactic' = $object.'kill_chain_phases'.'phase_name'
+            }
+        $TotalObjects = New-Object PSCustomObject -Property $Props
+        $Collection += $TotalObjects
+        }
+    
+    Write-Host "[++] Updating your Data Source sheet" -ForegroundColor Cyan	
+    $Collection | Select-Object  @{Name ="ID"; Expression={$_.ID -split "," }},@{Name ="Name"; Expression={$_.Name -join ","}},@{Name="Data Source";Expression={$_.'Data Source' -join ","}},@{Name="Platforms";Expression={$_.'Platforms' -join ","}},@{Name="Detection";Expression={$_.'Detection' -join ","}},@{Name="Description";Expression={$_.'Description' -join ","}},@{Name="Tactic";Expression={$_.'Tactic' -join ","}} | Sort-Object ID | Export-Excel $Excelfile -WorksheetName REF-DataSources      
+}
+
+function Request-ATTACKjson {
+    <#
+    .SYNOPSIS
+        Generates a JSON file to be imported into the ATT&CK Navigator. Based on a template and a filled Excel sheet
+        Requires module ImportExcel, Install it like this PS C:\> Install-Module ImportExcel
+    .DESCRIPTION
+        Generates a JSON file to be imported into the ATT&CK Navigator. The mitre_data_assessment Excel file contains all Techniques, which can be updated via Invoke-ATTACK-UpdateExcel.
+        These techniques contain DataSources, which are individually scored with a weight. The DataSourceEventTypes need to be scored per environment.
+        This script multiplies the respective DataSource scores and adds them to a total technique score. The generation date is added to the description.
+        
+        This is all gathered into a JSON file which can be opened here;
+        https://mitre-attack.github.io/attack-navigator/enterprise/
+    .EXAMPLE
+        PS C:\> Request-ATTACKjson -Excelfile .\mitre_data_assessment.xlsx -Template .\template.json -Output 2019-03-23-ATTACKcoverage.json 
+    .INPUTS
+        Excelfile = The Excel file containing all Datasource scores, default is .\mitre_data_assessment.xlsx
+        Template = The ATT&CK Navigator JSON template, default is .\template.json
+    .OUTPUTS
+        Output = The name of the JSON file you want to generate for the ATT&CK Navigator, default is ATTACKcoverage.json
+    .NOTES
+    #>
+    
+        param (
+            [Parameter(Mandatory=$false,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=0)]
+            [string]
+            $Excelfile = 'mitre_data_assessment.xlsx',
+            [Parameter(Mandatory=$false,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=0)]
+            [string]
+            $Template = 'template.json',
+            [Parameter(Mandatory=$false,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=0)]
+            [string]
+            $Title = 'DataCoverage',        
+            [Parameter(Mandatory=$false,
+                       ValueFromPipelineByPropertyName=$true,
+                       Position=0)]
+            [string]
+            $Output = 'ATTACKcoverage.json'
+        )
+    
+    $lookup = Import-Excel $Excelfile -WorksheetName DataSourceEvents
+    $mitre = Import-Excel $Excelfile -WorksheetName TechniqueDataSourceWeights
+    $JSONtemplate = (Get-Content -Raw -Path $Template | ConvertFrom-Json)
+    $Date = (get-date).ToString("yyy-MM-dd")
+    $finalresult = @()
+    # main loop
+    foreach($line in $mitre)
+    {
+        # Collect info
+        $DataSources = $line."Data Source"
+        $weights = $line.Weight -split ";"
+        # Define Variables
+        $techniques = @()
+        $techniquescore = 0
+        # Comma-separated datasources
+        $i = 0;
+        foreach($ds in $DataSources -split ",")
+        {
+            # Collect info
+            $DataSourceEvents = $lookup | Where-Object DataSource -eq $ds
+            $weight = ($weights[$i])  
+            # Variables
+            $total = 0
+            $metas = @()
+            # Iterate over DataSourceEvents
+            foreach($f in $DataSourceEvents)
+            {
+                $total += ($f.Score * $weight)
+                $meta = New-Object PSCustomObject -Property @{
+                    "name"="$($ds):$($f.Event)";
+                    "value"="Score: $($f.Score * $weight)"
+                }
+                $metas += $meta
+            }       
+            # Create technique object with technique, datasource score and events in metadata per datasource
+            $technique = New-Object PSCustomObject -Property @{
+                "techniqueID"=$line.ID;
+                "score"=$total;
+                "metadata"=$metas
+            }
+            # Add Technique data to technique list
+            $techniques += $technique
+            # Add the technique datasource score to the technique total
+            $techniquescore += $total
+            $i += 1
+        }
+        $techniqueDSscore = 0
+        $techniqueMetadata = @()
+        $techniques | ForEach-Object -Begin {    
+        } -Process { 
+            $techniqueDSscore += $_.score
+            $techniqueMetadata += $_.metadata
+        }
+        $techniqueTotal = New-Object PSCustomObject -Property @{
+            "techniqueID"=$technique.techniqueID;
+            "score"=$techniqueDSscore;
+            "metadata"=$techniqueMetadata
+        }
+        $finalresult += $techniqueTotal
+    }
+    $JSONtemplate.name = $Title
+    $JSONtemplate.description = $Date
+    # Insert the generated techniques into the json template
+    $JSONtemplate.techniques = $finalresult
+    # Generate the ATT&CK navigator file
+    $JSONtemplate | ConvertTo-Json -Depth 5 | Out-File -Encoding ascii $Output
+}
+
+
